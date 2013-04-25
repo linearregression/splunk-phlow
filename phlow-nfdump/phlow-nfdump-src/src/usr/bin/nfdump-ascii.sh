bf9203aace6b5ca520fdf2ae54e210bd6db180ad
Tim <tkropp@ise.com>
2013-04-25 13:16:24 -0300

    Update nfdump-ascii.sh
    added some comments to script functionality

--- phlow-nfdump/phlow-nfdump-src/src/usr/bin/nfdump-ascii.sh
+++ phlow-nfdump/phlow-nfdump-src/src/usr/bin/nfdump-ascii.sh
@@ -3,11 +3,18 @@
 #set -x
 
 # CONFIGURATION
+# http://man7.org/linux/man-pages/man7/inotify.7.html
+# This defines the variables that are used further down in the script
 DIR="/var/log/nfcapd/"
 EVENTS="moved_to"
 FIFO="/tmp/inotify2.fifo"
 
 # FUNCTIONS
+# on_exit() and on_event()
+# on_event() creates date, time, file variables, runs nfdump -qr on the DIR defined above 
+# then takes the binary and converts it to CSV ASCII with date, time variables defined. 
+# iNotify provides these facilities
+
 on_exit() {
 kill $INOTIFY_PID
 rm $FIFO
